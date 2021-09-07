d6df6caa0f4a39d44981300741a05dea785536f8
ZappaSte <ste.zappasodi@gmail.com>
2021-09-07 18:47:15 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/SiwProgettoClinicaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoClinicaApplication.class, args);
+	}
+
+}

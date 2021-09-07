933a141067040f359e393b0b69963ef394c357b2
ZappaSte <ste.zappasodi@gmail.com>
2021-09-07 19:11:50 +0200

    0.2 Postgres Funziona

--- src/main/java/it/uniroma3/siw/model/Esame.java
+++ src/main/java/it/uniroma3/siw/model/Esame.java
@@ -1,5 +1,26 @@
 package it.uniroma3.siw.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Esame {
+	
+	/*DEFINIZIONE VARIABILI*/
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.TABLE)
+	private Long id;
+
+	@Column(nullable = false)
+	private LocalDateTime dataPrenotazione;
+	
+	@Column(nullable = false)
+	private LocalDateTime dataEsame;
 
 }
